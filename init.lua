28ee2c737179a4d3f217db91e752ef883a67ba35
Deftly <deftkz@gmail.com>
2022-09-24 10:25:12 -0700

    initial commit

---
+++ init.lua
@@ -0,0 +1,19 @@
+require "sidd.options"
+require "sidd.keymaps"
+require "sidd.plugins"
+require "sidd.colorscheme"
+require "sidd.cmp"
+require("sidd.lsp")
+require("sidd.telescope")
+require("sidd.treesitter")
+require("sidd.autopairs")
+require("sidd.comment")
+require("sidd.gitsigns")
+require("sidd.nvim-tree")
+require("sidd.bufferline")
+require("sidd.lualine")
+require("sidd.project")
+require("sidd.impatient")
+require("sidd.indentline")
+require("sidd.alpha")
+require("sidd.toggleterm")
