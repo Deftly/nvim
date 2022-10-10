967f2d199964c718ffd5d31c37426f85b7f3ea39
Deftly <deftkz@gmail.com>
2022-10-10 11:42:50 -0700

    parentheses for require statemetns

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
-require "sidd.options"
-require "sidd.keymaps"
-require "sidd.plugins"
-require "sidd.colorscheme"
-require "sidd.cmp"
+require("sidd.options")
+require("sidd.keymaps")
+require("sidd.plugins")
+require("sidd.colorscheme")
+require("sidd.cmp")
 require("sidd.lsp")
 require("sidd.telescope")
 require("sidd.treesitter")
