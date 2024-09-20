7d197f8813301779220c7c474752f64da75ffa62
Deftly <deftkz@gmail.com>
2024-09-20 16:01:11 -0700

    clangd + debug via nvim-dap

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.ui.alpha" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     -- { import = "lazyvim.plugins.extras.lang.rust" },
     -- { import = "lazyvim.plugins.extras.lang.terraform" },
     -- { import = "lazyvim.plugins.extras.coding.copilot" },
