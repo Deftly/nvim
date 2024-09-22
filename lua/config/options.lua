6bf5dcc736804222f8c2cf934aacf9ae47468738
Deftly <deftkz@gmail.com>
2024-09-22 10:02:48 -0700

    enable line wrap by default

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.swapfile = false
+vim.opt.wrap = true
 
 -- Fix conceal-level for certain files
 vim.api.nvim_create_autocmd("FileType", {
